d9caeecdb72d96497e0a90704c1b57c4486bb243
puneeth-007 <100269212+puneeth-007@users.noreply.github.com>
2025-02-27 10:01:39 +0530

    fin

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ with open(r'C:\Users\punee\OneDrive\Desktop\python\Internship\key.txt.txt') as f
 
 llm= ChatGoogleGenerativeAI(model="gemini-2.0-pro-exp",api_key=api)
 prompt_template=ChatPromptTemplate.from_messages([('system','''
-You are an AI travel planner that takes source and destination and provides the detailed travel plan'''),('human','{prompt}')])
+You are an AI travel planner that takes source and destination and provides the detailed travel plan, when starting the message start as Travel plan from source to destination provided by user'''),('human','{prompt}')])
 
 chain=prompt_template | llm
 
@@ -20,19 +20,14 @@ source=st.text_input('Enter the source location',key='source')
 
 destination=st.text_input('Enter the destination location',key='destination')
 
-st.write(source)
-st.write(destination)
-
 user='I am travelling from '+source+ 'to '+destination+' provide me travel plan with cost, time and if possible different modes of travel available with detailed table as well'
 
 if st.button('Submit'):
-    input={'prompt':prompt}
-    st.write(source)
-    st.write(destination)
-    #response=chain.invoke(input)
+    input={'prompt':user}
+    response=chain.invoke(input)
 
-    #content=response.content
+    content=response.content
 
-    #st.markdown(content)
+    st.markdown(content)
 
 
